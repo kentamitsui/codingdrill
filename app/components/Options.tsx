939a8a14590479b3d1fe275725c5ecbfa9aef0b9
kenta_mitsui <kentam12@gmail.com>
2024-09-14 06:39:55 +0900

    fix: topicデータを出力する際、条件分岐を行うよう設定 型定義の修正

---
+++ app/components/Options.tsx
@@ -0,0 +1,43 @@
+import { LanguageSelectProps } from "../type/type";
+
+export const Options: React.FC<LanguageSelectProps> = ({
+  label,
+  data,
+  name,
+  id,
+  defaultSelected,
+}) => {
+  return (
+    <>
+      <label htmlFor={label}></label>
+      <select
+        className="m-1 cursor-pointer rounded-md bg-gray-200 p-1 duration-300 hover:bg-gray-400 dark:bg-menu dark:hover:bg-slate-700"
+        name={name}
+        id={id}
+      >
+        <option selected disabled className="text-start">
+          {defaultSelected}
+        </option>
+        {label === "select-topic"
+          ? // `optgroup`の構造をループで出力する
+            Object.entries(data).map(([groupLabel, options]) => (
+              <optgroup key={groupLabel} label={groupLabel}>
+                {Object.entries(options).map(([key, value]) => (
+                  <option key={key} value={key}>
+                    {value}
+                  </option>
+                ))}
+              </optgroup>
+            ))
+          : // 通常のオプションリストを表示
+            Object.entries(data).map(([key, value]) => (
+              <option key={key} value={key}>
+                {typeof value === "string" ? value : ""}
+              </option>
+            ))}
+      </select>
+    </>
+  );
+};
+
+export default Options;
