84944df090513aefa40872108079ee22e51a6e2b
kenta_mitsui <kentam12@gmail.com>
2024-09-18 01:43:27 +0900

    add: splitter.jsの導入

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -1,10 +1,13 @@
 import Sidebar from "./Sidebar";
+import "../../app/globals.css";
+import Split_Horizontal from "../feature/Splitter";
 
 export default function Main() {
   return (
     <main className="my-2 flex-grow overflow-hidden">
       <div className="flex h-full">
         <Sidebar />
+        <Split_Horizontal />
       </div>
     </main>
   );
