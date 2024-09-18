faf704a3245acd2ec67f6d80c1c7ceb15235969d
kenta_mitsui <kentam12@gmail.com>
2024-09-18 15:04:39 +0900

    add: ダークモードの追加 イコンのクリックでカラーモードが切り替わるよう調整

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
-import type { Metadata } from 'next'
-import "./globals.css"
+import type { Metadata } from "next";
+import "./globals.css";
+import { ThemeProvider } from "next-themes";
 
 export const metadata: Metadata = {
-  title: 'Create Next App',
-  description: 'create a coding test problem',
-}
+  title: "Create Next App",
+  description: "create a coding test problem",
+};
 
 export default function RootLayout({
   children,
@@ -17,21 +18,12 @@ export default function RootLayout({
         <meta charSet="UTF-8" />
         <meta http-equiv="Content-Type" content="charset=utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <script
-          src="https://cdnjs.cloudflare.com/ajax/libs/split.js/1.6.5/split.min.js"
-          integrity="sha512-lNjb0qWDVvt1zfSiXufaxtlFtenve3BLbvljxuMXuSr0DE0HYp5OhX0u89uwNd6MvlX1bgJ8ulfG4JMGurs8UA=="
-          crossOrigin="anonymous"
-          referrerPolicy="no-referrer"
-          async
-        />
       </head>
-      {children}
-      <script src="localstorage.js" async />
-      <script src="../node_modules/monaco-editor/min/vs/loader.js" async />
-      <script src="monacoEditor.js" async />
-      <script src="splitter.js" async />
-      <script src="colorTheme.js" async />
-      <script src="script.js" async />
+      <body className="mx-auto flex h-screen max-w-[1920px] flex-col bg-[#6b7280] p-5 text-black dark:bg-[#1e1e1e] dark:text-[#d4d4d4]">
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
