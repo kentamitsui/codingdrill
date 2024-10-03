f2a96854901703b8d61da18dc26592a2e209cdd0
kenta_mitsui <kentam12@gmail.com>
2024-10-03 14:45:54 +0900

    fix: APIリクエスト時、ボタンを押せないよう修正 マウスオーバー時のカーソルを変更 クリックの可否を真偽で管理 不必要なidの削除

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -12,7 +12,7 @@ export const Button: React.FC<ButtonProps> = ({
       id={id}
       type={type}
       className={`m-1 rounded-[15px] bg-gray-400 p-1 text-[1rem] font-bold duration-300 hover:bg-gray-600 dark:bg-slate-700 dark:hover:bg-slate-500 ${
-        clicked === true ? "cursor-not-allowed opacity-50" : null
+        clicked === true ? "cursor-not-allowed opacity-50" : ""
       }`}
       onClick={onClick}
       disabled={clicked}
--- app/type/type.ts
+++ app/type/type.ts
@@ -1,5 +1,3 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { NextRequest } from "next/server";
 import { Dispatch, SetStateAction } from "react";
 
 // 選択タグに対する型定義
@@ -7,7 +5,7 @@ export interface SelectProps {
   label?: string;
   data: { [key: string]: { [key: string]: string } | string }; // `optgroup`内に`option`を持つ可能性があるため修正
   name: string; // セレクトボックスのname属性
-  id: string; // セレクトボックスのid属性
+  disabled: boolean;
   defaultSelected?: string; // デフォルトのラベルを可変にするためのプロップ
   setSelected: Dispatch<SetStateAction<string>>;
 }
