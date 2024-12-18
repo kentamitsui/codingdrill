8dc79d2e528395fe68be3ed6d12380e80c2be54e
kenta_mitsui <kentam12@gmail.com>
2024-12-19 04:41:04 +0900

    fix: プロンプト修正 問題文の出力内容変更 レイアウト修正

--- app/components/ProblemSection.tsx
+++ app/components/ProblemSection.tsx
@@ -15,21 +15,74 @@ const ProblemSection: React.FC = () => {
     const description = jsonFormattedProblemContent
       ? `Description\n${jsonFormattedProblemContent.problemStatement}\n\n`
       : "";
+
+    const functionSignature = jsonFormattedProblemContent
+      ? `function Signature\n${jsonFormattedProblemContent.functionSignature}\n\n`
+      : "";
+
+    const inputFormat = jsonFormattedProblemContent
+      ? `inputFormat\n${jsonFormattedProblemContent.inputFormat}\n\n`
+      : "";
+
+    const outputFormat = jsonFormattedProblemContent
+      ? `outputFormat\n${jsonFormattedProblemContent.outputFormat}\n\n`
+      : "";
+
+    const size = jsonFormattedProblemContent
+      ? `size\n${jsonFormattedProblemContent.constraints.size}\n\n`
+      : "";
+
+    const valueRange = jsonFormattedProblemContent
+      ? `valueRange\n${jsonFormattedProblemContent.constraints.valueRange}\n\n`
+      : "";
+
+    const kRange = jsonFormattedProblemContent
+      ? `kRange\n${jsonFormattedProblemContent.constraints.kRange}\n\n`
+      : "";
+
     const example1 = jsonFormattedProblemContent
       ? `Example 1\nInput: ${jsonFormattedProblemContent.example1.input}\nOutput: ${jsonFormattedProblemContent.example1.output}\nExplanation: ${jsonFormattedProblemContent.example1.explanation}\n\n`
       : "";
+
     const example2 = jsonFormattedProblemContent
       ? `Example 2\nInput: ${jsonFormattedProblemContent.example2.input}\nOutput: ${jsonFormattedProblemContent.example2.output}\nExplanation: ${jsonFormattedProblemContent.example2.explanation}\n\n`
       : "";
+
     const example3 = jsonFormattedProblemContent
       ? `Example 3\nInput: ${jsonFormattedProblemContent.example3.input}\nOutput: ${jsonFormattedProblemContent.example3.output}\nExplanation: ${jsonFormattedProblemContent.example3.explanation}\n\n`
       : "";
-    const notes = jsonFormattedProblemContent
-      ? `Notes\nTime Complexity: ${jsonFormattedProblemContent.notes.timeComplexity}\nSpace Complexity: ${jsonFormattedProblemContent.notes.spaceComplexity}\nEdge Cases: ${jsonFormattedProblemContent.notes.edgeCases}\nOther Consideration: ${jsonFormattedProblemContent.notes.otherConsiderations}`
+
+    const edgeCase1 = jsonFormattedProblemContent
+      ? `edgeCase 1\nInput: ${jsonFormattedProblemContent.edgeCase1.input}\nOutput: ${jsonFormattedProblemContent.edgeCase1.output}\nExplanation: ${jsonFormattedProblemContent.edgeCase1.explanation}\n\n`
+      : "";
+
+    const edgeCase2 = jsonFormattedProblemContent
+      ? `edgeCase 2\nInput: ${jsonFormattedProblemContent.edgeCase2.input}\nOutput: ${jsonFormattedProblemContent.edgeCase2.output}\nExplanation: ${jsonFormattedProblemContent.edgeCase2.explanation}\n\n`
+      : "";
+
+    const analysis = jsonFormattedProblemContent
+      ? `analysis\nTime Complexity: ${jsonFormattedProblemContent.analysis.timeComplexity}\nSpace Complexity: ${jsonFormattedProblemContent.analysis.spaceComplexity}\nEdge Cases: ${jsonFormattedProblemContent.analysis.edgeCases}\nOther Consideration: ${jsonFormattedProblemContent.analysis.otherConsiderations}`
+      : "";
+
+    const hints = jsonFormattedProblemContent
+      ? `hints\n ${jsonFormattedProblemContent.hints}`
       : "";
 
     const formattedProblemContent =
-      description + example1 + example2 + example3 + notes;
+      description +
+      functionSignature +
+      inputFormat +
+      outputFormat +
+      size +
+      valueRange +
+      kRange +
+      example1 +
+      example2 +
+      example3 +
+      edgeCase1 +
+      edgeCase2 +
+      analysis +
+      hints;
 
     setFormattedProblemContent(formattedProblemContent);
   });
@@ -64,95 +117,212 @@ const ProblemSection: React.FC = () => {
         </button>
       </div>
       <div className="p-[15px_30px] text-[16px] leading-normal tracking-wider width_1440px:text-[18px] width_1680px:text-[20px]">
-        <div className="whitespace-break-spaces text-[20px] font-medium width_1440px:text-[22px] width_1680px:text-[24px]">
-          {jsonFormattedProblemContent
-            ? "Description\n" + jsonFormattedProblemContent?.problemStatement
-            : null}
-        </div>
-        <div hidden></div>
-        <div className="mt-[30px] text-[20px] font-medium width_1440px:text-[22px] width_1680px:text-[24px]">
-          {jsonFormattedProblemContent ? "Example 1" : null}
-        </div>
-        <div className="font-light">
-          {jsonFormattedProblemContent
-            ? "Input: " + jsonFormattedProblemContent?.example1.input
-            : null}
-        </div>
-        <div className="font-light">
-          {jsonFormattedProblemContent
-            ? "Output: " + jsonFormattedProblemContent?.example1.output
-            : null}
-        </div>
-        <div className="font-light">
-          {jsonFormattedProblemContent
-            ? "Explanation: " +
-              jsonFormattedProblemContent?.example1.explanation
-            : null}
-        </div>
-        <div className="mt-[30px] text-[20px] font-medium width_1440px:text-[22px] width_1680px:text-[24px]">
-          {jsonFormattedProblemContent ? "Example 2" : null}
-        </div>
-        <div className="font-light">
-          {jsonFormattedProblemContent
-            ? "Input: " + jsonFormattedProblemContent?.example2.input
-            : null}
-        </div>
-        <div className="font-light">
-          {jsonFormattedProblemContent
-            ? "Output: " + jsonFormattedProblemContent?.example2.output
-            : null}
-        </div>
-        <div className="font-light">
-          {jsonFormattedProblemContent
-            ? "Explanation: " +
-              jsonFormattedProblemContent?.example2.explanation
-            : null}
-        </div>
-        <div className="mt-[30px] text-[20px] font-medium width_1440px:text-[22px] width_1680px:text-[24px]">
-          {jsonFormattedProblemContent ? "Example 3" : null}
-        </div>
-        <div className="font-light">
-          {jsonFormattedProblemContent
-            ? "Input: " + jsonFormattedProblemContent?.example3.input
-            : null}
-        </div>
-        <div className="font-light">
-          {jsonFormattedProblemContent
-            ? "Output: " + jsonFormattedProblemContent?.example3.output
-            : null}
-        </div>
-        <div className="font-light">
-          {jsonFormattedProblemContent
-            ? "Explanation: " +
-              jsonFormattedProblemContent?.example3.explanation
-            : null}
-        </div>
-        <div className="mt-[30px] text-[20px] font-medium width_1440px:text-[22px] width_1680px:text-[24px]">
-          {jsonFormattedProblemContent ? "Notes" : null}
-        </div>
-        <div className="grid gap-[10px]">
-          <div className="font-light">
-            {jsonFormattedProblemContent
-              ? "Time Complexity: " +
-                jsonFormattedProblemContent?.notes.timeComplexity
-              : null}
+        <div className="grid gap-5 whitespace-break-spaces">
+          <div>
+            <p className="text-[20px] font-medium width_1440px:text-[22px] width_1680px:text-[24px]">
+              {jsonFormattedProblemContent ? "Description\n" : null}
+            </p>
+            <p className="font-light">
+              {jsonFormattedProblemContent
+                ? jsonFormattedProblemContent.problemStatement + "\n"
+                : null}
+            </p>
           </div>
-          <div className="font-light">
-            {jsonFormattedProblemContent
-              ? "Space Complexity: " +
-                jsonFormattedProblemContent?.notes.spaceComplexity
-              : null}
+          <div>
+            <p className="text-[20px] font-medium width_1440px:text-[22px] width_1680px:text-[24px]">
+              {jsonFormattedProblemContent ? "Function Signature\n" : null}
+            </p>
+            <p className="font-light">
+              {jsonFormattedProblemContent
+                ? jsonFormattedProblemContent.functionSignature + "\n"
+                : null}
+            </p>
           </div>
-          <div className="font-light">
-            {jsonFormattedProblemContent
-              ? "Edge Cases: " + jsonFormattedProblemContent?.notes.edgeCases
-              : null}
+          <div>
+            <p className="text-[20px] font-medium width_1440px:text-[22px] width_1680px:text-[24px]">
+              {jsonFormattedProblemContent ? "Input Format\n" : null}
+            </p>
+            <p className="font-light">
+              {jsonFormattedProblemContent
+                ? jsonFormattedProblemContent.inputFormat + "\n"
+                : null}
+            </p>
           </div>
-          <div className="font-light">
-            {jsonFormattedProblemContent
-              ? "Other Consideration: " +
-                jsonFormattedProblemContent?.notes.otherConsiderations
-              : null}
+          <div>
+            <p className="text-[20px] font-medium width_1440px:text-[22px] width_1680px:text-[24px]">
+              {jsonFormattedProblemContent ? "Output Format\n" : null}
+            </p>
+            <p className="font-light">
+              {jsonFormattedProblemContent
+                ? jsonFormattedProblemContent.outputFormat + "\n"
+                : null}
+            </p>
+          </div>
+          <div className="grid gap-[5px]">
+            <p className="text-[20px] font-medium width_1440px:text-[22px] width_1680px:text-[24px]">
+              {jsonFormattedProblemContent ? "Constraints\n" : null}
+            </p>
+            <p className="font-light">
+              {jsonFormattedProblemContent
+                ? "Size: " + jsonFormattedProblemContent.constraints.size + "\n"
+                : null}
+              {jsonFormattedProblemContent
+                ? "Value Range: " +
+                  jsonFormattedProblemContent.constraints.valueRange +
+                  "\n"
+                : null}
+              {jsonFormattedProblemContent
+                ? "K Range: " +
+                  jsonFormattedProblemContent.constraints.kRange +
+                  "\n"
+                : null}
+            </p>
+          </div>
+          <div className="mt-8 grid gap-[5px]">
+            <p className="text-[20px] font-medium width_1440px:text-[22px] width_1680px:text-[24px]">
+              {jsonFormattedProblemContent ? "Example 1" : null}
+            </p>
+            <p className="font-light">
+              {jsonFormattedProblemContent
+                ? "Input: " + jsonFormattedProblemContent?.example1.input
+                : null}
+            </p>
+            <p className="font-light">
+              {jsonFormattedProblemContent
+                ? "Output: " + jsonFormattedProblemContent?.example1.output
+                : null}
+            </p>
+            <p className="font-light">
+              {jsonFormattedProblemContent
+                ? "Explanation: " +
+                  jsonFormattedProblemContent?.example1.explanation
+                : null}
+            </p>
+          </div>
+          <div className="grid gap-[5px]">
+            <p className="text-[20px] font-medium width_1440px:text-[22px] width_1680px:text-[24px]">
+              {jsonFormattedProblemContent ? "Example 2" : null}
+            </p>
+            <p className="font-light">
+              {jsonFormattedProblemContent
+                ? "Input: " + jsonFormattedProblemContent?.example2.input
+                : null}
+            </p>
+            <p className="font-light">
+              {jsonFormattedProblemContent
+                ? "Output: " + jsonFormattedProblemContent?.example2.output
+                : null}
+            </p>
+            <p className="font-light">
+              {jsonFormattedProblemContent
+                ? "Explanation: " +
+                  jsonFormattedProblemContent?.example2.explanation
+                : null}
+            </p>
+          </div>
+          <div className="grid gap-[5px]">
+            <p className="text-[20px] font-medium width_1440px:text-[22px] width_1680px:text-[24px]">
+              {jsonFormattedProblemContent ? "Example 3" : null}
+            </p>
+            <p className="font-light">
+              {jsonFormattedProblemContent
+                ? "Input: " + jsonFormattedProblemContent?.example3.input
+                : null}
+            </p>
+            <p className="font-light">
+              {jsonFormattedProblemContent
+                ? "Output: " + jsonFormattedProblemContent?.example3.output
+                : null}
+            </p>
+            <p className="font-light">
+              {jsonFormattedProblemContent
+                ? "Explanation: " +
+                  jsonFormattedProblemContent?.example3.explanation
+                : null}
+            </p>
+          </div>
+          <div className="grid gap-[5px]">
+            <p className="text-[20px] font-medium width_1440px:text-[22px] width_1680px:text-[24px]">
+              {jsonFormattedProblemContent ? "Edge Case 1" : null}
+            </p>
+            <p className="font-light">
+              {jsonFormattedProblemContent
+                ? "Input: " + jsonFormattedProblemContent?.edgeCase1.input
+                : null}
+            </p>
+            <p className="font-light">
+              {jsonFormattedProblemContent
+                ? "Output: " + jsonFormattedProblemContent?.edgeCase1.output
+                : null}
+            </p>
+            <p className="font-light">
+              {jsonFormattedProblemContent
+                ? "Explanation: " +
+                  jsonFormattedProblemContent?.edgeCase1.explanation
+                : null}
+            </p>
+          </div>
+          <div className="grid gap-[5px]">
+            <p className="text-[20px] font-medium width_1440px:text-[22px] width_1680px:text-[24px]">
+              {jsonFormattedProblemContent ? "Edge Case 2" : null}
+            </p>
+            <p className="font-light">
+              {jsonFormattedProblemContent
+                ? "Input: " + jsonFormattedProblemContent?.edgeCase2.input
+                : null}
+            </p>
+            <p className="font-light">
+              {jsonFormattedProblemContent
+                ? "Output: " + jsonFormattedProblemContent?.edgeCase2.output
+                : null}
+            </p>
+            <p className="font-light">
+              {jsonFormattedProblemContent
+                ? "Explanation: " +
+                  jsonFormattedProblemContent?.edgeCase2.explanation
+                : null}
+            </p>
+          </div>
+          <div>
+            <p className="mt-8 text-[20px] font-medium width_1440px:text-[22px] width_1680px:text-[24px]">
+              {jsonFormattedProblemContent ? "Analysis" : null}
+            </p>
+            <div className="grid gap-[5px]">
+              <p className="font-light">
+                {jsonFormattedProblemContent
+                  ? "Time Complexity: " +
+                    jsonFormattedProblemContent?.analysis.timeComplexity
+                  : null}
+              </p>
+              <p className="font-light">
+                {jsonFormattedProblemContent
+                  ? "Space Complexity: " +
+                    jsonFormattedProblemContent?.analysis.spaceComplexity
+                  : null}
+              </p>
+              <p className="font-light">
+                {jsonFormattedProblemContent
+                  ? "Edge Cases: " +
+                    jsonFormattedProblemContent?.analysis.edgeCases
+                  : null}
+              </p>
+              <p className="font-light">
+                {jsonFormattedProblemContent
+                  ? "Other Consideration: " +
+                    jsonFormattedProblemContent?.analysis.otherConsiderations
+                  : null}
+              </p>
+            </div>
+          </div>
+          <div>
+            <p className="mt-8 text-[20px] font-medium width_1440px:text-[22px] width_1680px:text-[24px]">
+              {jsonFormattedProblemContent ? "Hints" : null}
+            </p>
+            <p className="font-light">
+              {jsonFormattedProblemContent
+                ? jsonFormattedProblemContent?.hints
+                : null}
+            </p>
           </div>
         </div>
       </div>
