cd1e1026d65d553d23b0465ed9a343c27b5f22fb
kenta_mitsui <kentam12@gmail.com>
2024-12-22 15:41:24 +0900

    feat: ボタンコンポーネントに選択肢の状態管理を追加し、全ての選択肢が選択されていない場合にボタンを無効化

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,8 +1,34 @@
 import { useAppContext } from "./AppContext";
 import { ButtonProps } from "../type/type";
+import { useEffect, useState } from "react";
 
 export default function Button({ id, type, text, onClick }: ButtonProps) {
-  const { isDisabled } = useAppContext();
+  const {
+    isDisabled,
+    difficulty,
+    dataType,
+    topic,
+    selectedLanguage,
+    isCreateProblem,
+  } = useAppContext();
+  // 選択肢が全て選択されたかどうかの状態管理に使用
+  const [isAllSelected, setIsAllSelected] = useState(false);
+
+  // 全ての選択肢が選択されたかどうかを判定する
+  useEffect(() => {
+    if (
+      difficulty !== "" &&
+      dataType !== "" &&
+      topic !== "" &&
+      selectedLanguage !== ""
+    ) {
+      setIsAllSelected(true);
+    } else {
+      setIsAllSelected(false);
+    }
+  }, [difficulty, dataType, topic, selectedLanguage]);
+
+  console.log(isCreateProblem);
 
   return (
     <button
@@ -10,9 +36,9 @@ export default function Button({ id, type, text, onClick }: ButtonProps) {
       type={type}
       className={`w-full rounded-[15px] bg-gray-400 p-1 text-[14px] font-bold duration-300 hover:bg-gray-600 dark:bg-slate-700 dark:hover:bg-slate-500 ${
         isDisabled === true ? "cursor-not-allowed opacity-50" : ""
-      }`}
+      } ${isAllSelected === false ? "cursor-not-allowed opacity-50" : ""}`}
       onClick={onClick}
-      disabled={isDisabled}
+      disabled={isDisabled || !isAllSelected}
     >
       {text}
     </button>
