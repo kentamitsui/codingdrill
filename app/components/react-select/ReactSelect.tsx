f3842443e56c6e3ae6b95a9d2564f2926f1adc83
kenta_mitsui <kentam12@gmail.com>
2025-01-07 14:57:19 +0900

    feat: ReactSelectコンポーネントのスタイルを改善し、無効化状態の管理を追加 fix: optionタグの背景色等を修正

--- app/components/react-select/ReactSelect.tsx
+++ app/components/react-select/ReactSelect.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import Select from "react-select";
 import { ReactSelectProps } from "@/app/type/type";
 import "../../globals.css";
+import { useAppContext } from "../AppContext";
 
 export default function ReactSelect({
   currentSelectedSavedData,
-  isDisabled,
   handleChangeSavedData,
   saveData,
   currentTheme,
 }: ReactSelectProps) {
+  const { isDisabled } = useAppContext();
   // オプションのデータをreact-select用に変換
   const options =
     saveData &&
@@ -35,19 +36,27 @@ export default function ReactSelect({
 
   // スタイルのカスタマイズ
   interface CustomStyles {
-    control: (provided: any) => any;
+    control: (provided: any, state: any) => any;
+    valueContainer: (provided: any) => any;
+    input: (provided: any) => any;
+    indicatorsContainer: (provided: any) => any;
+    indicatorSeparator: (provided: any) => any;
+    dropdownIndicator: (provided: any) => any;
     singleValue: (provided: any) => any;
     placeholder: (provided: any) => any;
     menu: (provided: any) => any;
     menuList: (provided: any) => any;
+    option: (provided: any, state: any) => any;
   }
 
   const customStyles: CustomStyles = {
-    control: (provided) => ({
+    control: (provided, state) => ({
       ...provided,
-      borderRadius: "0.375rem", // rounded-md
-      padding: "0.25rem", // p-1
-      transition: "background-color 300ms",
+      height: "20px",
+      minHeight: "30px",
+      borderRadius: "6px", // rounded-md
+      padding: "4px",
+      transition: "300ms",
       backgroundColor:
         currentSelectedSavedData === "" && currentTheme === "dark"
           ? "#0D1117"
@@ -58,29 +67,89 @@ export default function ReactSelect({
               : currentSelectedSavedData !== "" && currentTheme === "light"
                 ? "#9ca3af"
                 : "",
-      cursor: isDisabled ? "not-allowed" : "pointer",
-      opacity: isDisabled ? 0.5 : 1,
+      borderColor: currentTheme === "dark" ? "#0D1117" : "#e5e7eb",
+      pointerEvents: state.isDisabled ? "auto" : "auto", // ポインターイベントを上書きして有効化
+      opacity: state.isDisabled ? 0.5 : 1,
+      cursor: state.isDisabled ? "not-allowed" : "pointer",
       "&:hover": {
         backgroundColor: currentTheme === "dark" ? "#334155" : "#9CA3AF",
-        opacity: currentSelectedSavedData !== "" ? 0.5 : 1,
+        opacity: isDisabled || currentSelectedSavedData !== "" ? 0.5 : 1,
       },
+    }),
+    valueContainer: (provided) => ({
+      ...provided,
+      height: "20px",
+      padding: "0px",
+      margin: "0px",
+    }),
+    input: (provided) => ({
+      ...provided,
+      height: "20px",
+      padding: "0px",
+      margin: "0px",
+    }),
+    indicatorsContainer: (provided) => ({
+      ...provided,
+      height: "20px",
+    }),
+    indicatorSeparator: (provided) => ({
+      ...provided,
+      display: "none",
+    }),
+    dropdownIndicator: (provided) => ({
+      ...provided,
+      height: "20px",
+      paddingTop: "0px",
+      paddingBottom: "0px",
     }),
     singleValue: (provided) => ({
       ...provided,
       textAlign: "start",
+      margin: "0px",
+      color: currentTheme === "dark" ? "#d4d4d4" : "#000000", // 文字色
     }),
     placeholder: (provided) => ({
       ...provided,
-      color: "#6B7280", // text-gray-500
+      height: "20px",
+      margin: "0px",
+      color: currentTheme === "dark" ? "#d4d4d4" : "#000000", // 文字色
     }),
     menu: (provided) => ({
       ...provided,
       width: "350px", // ドロップダウンメニュー全体の幅を指定
+      borderRadius: "8px", // rounded-md
+      color: currentTheme === "dark" ? "#d4d4d4" : "#000000", // 文字色
     }),
     menuList: (provided) => ({
       ...provided,
       width: "100%", // メニュー内のリストの幅を親要素(menu)に合わせる
       textAlign: "left", // 必要に応じてカスタマイズ
+      padding: "0px",
+      borderRadius: "6px", // rounded-md
+      color: currentTheme === "dark" ? "#d4d4d4" : "#000000", // 文字色
+    }),
+    option: (provided, state) => ({
+      ...provided,
+      whiteSpace: "break-spaces",
+      backgroundColor: state.isSelected
+        ? currentTheme === "dark"
+          ? "#334155"
+          : "#e5e7eb" // 選択時の背景色
+        : state.isFocused
+          ? currentTheme === "dark"
+            ? "#475569"
+            : "#f3f4f6" // ホバー時の背景色
+          : currentTheme === "dark"
+            ? "#1f2937"
+            : "#ffffff", // 通常時の背景色
+      color: state.isSelected
+        ? currentTheme === "dark"
+          ? "#d4d4d4"
+          : "#000000" // 選択時の文字色
+        : currentTheme === "dark"
+          ? "#d4d4d4"
+          : "#000000", // 通常時の文字色
+      cursor: isDisabled ? "not-allowed" : "pointer",
     }),
   };
 
@@ -88,15 +157,13 @@ export default function ReactSelect({
     <Select
       name="data"
       id="saveData"
-      className="whitespace-break-spaces"
       classNamePrefix="react-select"
       value={selectedOption}
       onChange={handleChangeSavedData}
-      // openMenuOnClick={""}
       options={options && options.length > 0 ? options : [placeholderOption]}
-      isDisabled={isDisabled}
-      placeholder="Select Save Data"
+      placeholder="Save Data"
       styles={customStyles}
+      isDisabled={isDisabled} // 無効化を適用
       isClearable
     />
   );
