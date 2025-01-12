0e5dced04cb2e66366ab9415b0d445b29e86891e
kenta_mitsui <kentam12@gmail.com>
2025-01-12 14:37:07 +0900

    feaure: ResetButtonコンポーネントにSVGアイコンを追加 テーマに応じて切り替えるよう修正 Optionコンポーネント右横に画像を追加

--- app/components/ui/header/Header.tsx
+++ app/components/ui/header/Header.tsx
@@ -1,5 +1,6 @@
 import ThemeSwitch from "@/app/feature/darkmode/ThemeSwitch";
 import ResetButton from "@/app/feature/splitter/components/ui/ResetButton";
+import menuData from "@/app/config/config.json";
 
 export default function Header() {
   return (
@@ -7,7 +8,11 @@ export default function Header() {
       <h1 className="text-xl">Coding Drill</h1>
       <div className="ml-auto flex items-center justify-center">
         <ThemeSwitch />
-        <ResetButton />
+        <ResetButton
+          lightSvgSrc={menuData.svgIcon.resetLight}
+          darkSvgSrc={menuData.svgIcon.resetDark}
+          altText="reset layout button"
+        />
       </div>
     </header>
   );
