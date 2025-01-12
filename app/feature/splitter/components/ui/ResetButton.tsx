c152658ea12ddccb6f72b0e3457c71a35a229e9b
kenta_mitsui <kentam12@gmail.com>
2025-01-12 14:37:07 +0900

    feaure: ResetButtonコンポーネントにSVGアイコンを追加 テーマに応じて切り替えるよう修正 Optionコンポーネント右横に画像を追加

--- app/feature/splitter/components/ui/ResetButton.tsx
+++ app/feature/splitter/components/ui/ResetButton.tsx
@@ -1,7 +1,20 @@
+import { useAppContext } from "@/app/context/AppContext";
 import { usePanelContext } from "@/app/feature/splitter/context/PanelContext";
+import Image from "next/image";
+
+interface ResetButtonProps {
+  lightSvgSrc: string;
+  darkSvgSrc: string;
+  altText: string;
+}
 
 // パネルサイズをリセットするボタン
-export default function ResetButton() {
+export default function ResetButton({
+  lightSvgSrc,
+  darkSvgSrc,
+  altText,
+}: ResetButtonProps) {
+  const { currentTheme } = useAppContext();
   const { resetHorizontalPanelSizes, resetVerticalPanelSizes } =
     usePanelContext();
 
@@ -14,9 +27,16 @@ export default function ResetButton() {
   return (
     <button
       onClick={handlePanelSizeReset}
-      className="w-[120px] rounded-md bg-gray-400 p-1 font-bold duration-300 hover:bg-gray-600 dark:bg-slate-700 dark:hover:bg-slate-500"
+      className="flex w-[120px] items-center justify-center rounded-md bg-gray-400 p-1 font-bold duration-300 hover:bg-gray-600 dark:bg-slate-700 dark:hover:bg-slate-500"
     >
-      reset size
+      Restore
+      <Image
+        className="ml-2"
+        src={currentTheme === "dark" ? lightSvgSrc : darkSvgSrc}
+        alt={altText}
+        width={20}
+        height={20}
+      />
     </button>
   );
 }
