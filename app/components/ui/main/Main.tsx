b39ea08fb3cd0a3117f3e155dddc60077a8eb70c
kenta_mitsui <kentam12@gmail.com>
2025-01-15 01:03:29 +0900

    add: SVG画像、フッター追加 fix: READMEを更新中

--- app/components/ui/main/Main.tsx
+++ app/components/ui/main/Main.tsx
@@ -4,11 +4,17 @@ import Split_Horizontal from "@/app/feature/splitter/Splitter";
 
 export default function Main() {
   return (
-    <main className="my-2 flex-grow overflow-hidden">
-      <div className="flex h-full">
-        <Sidebar />
-        <Split_Horizontal />
-      </div>
-    </main>
+    <>
+      <main className="my-2 flex-grow overflow-hidden">
+        <div className="flex h-full">
+          <Sidebar />
+          <Split_Horizontal />
+        </div>
+        <footer className="text-3xl">test</footer>
+      </main>
+      <footer className="bg:opacity-0 text-center text-[12px]">
+        © 2025 CodingDrill. All rights reserved.
+      </footer>
+    </>
   );
 }
