c2a6b2f58e1f3e21bfe4cefb8fd188c1b74d4535
kenta_mitsui <kentam12@gmail.com>
2025-01-23 23:51:02 +0900

    refactor: コンポーネント名を変更し、アロー関数に統一

--- app/components/ui/main/Main.tsx
+++ app/components/ui/main/Main.tsx
@@ -1,6 +1,6 @@
 import Sidebar from "@/app/components/layout/sidebar/Sidebar";
 import "@/app/styles/globals.css";
-import Split_Horizontal from "@/app/feature/splitter/Splitter";
+import SplitHorizontal from "@/app/feature/splitter/Splitter";
 import Footer from "@/app/components/ui/footer/Footer";
 
 export default function Main() {
@@ -9,7 +9,7 @@ export default function Main() {
       <main className="my-2 flex-grow overflow-hidden">
         <div className="flex h-full">
           <Sidebar />
-          <Split_Horizontal />
+          <SplitHorizontal />
         </div>
         <footer className="text-3xl">test</footer>
       </main>
