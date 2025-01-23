2aece53a42ca188e8e7ad5b737906101fa9f4eea
kenta_mitsui <kentam12@gmail.com>
2025-01-24 00:24:57 +0900

    refactor: コンポーネントの移動

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { PanelProvider } from "@/app/feature/splitter/context/PanelContext";
 import { SelectedDataProvider } from "@/app/context/AppContext";
 import { LocalStorageProvider } from "@/app/feature/storage/context/StorageContext";
 import Header from "@/app/components/ui/header/Header";
-import Main from "@/app/components/ui/main/Main";
+import Main from "@/app/components/main/Main";
 
 export default function Page() {
   return (
