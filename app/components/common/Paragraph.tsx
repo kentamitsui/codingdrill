16139eca3f68007ad04a4d3f6fb953825f5f7b56
kenta_mitsui <kentam12@gmail.com>
2025-02-02 03:05:18 +0900

    feat: 表示する文章を、偶数行で改行するよう改善

--- app/components/common/Paragraph.tsx
+++ app/components/common/Paragraph.tsx
@@ -6,6 +6,29 @@ const Paragraph: React.FC<QuestionParagraphProps | ReviewParagraphProps> = ({
   titleText,
   paragraphContent,
 }) => {
+  // テキストを2行ずつに分割する関数
+  const lineBreaks = (text: string, lineSizes: number = 2): string[] => {
+    // 日本語では"。 "が文末にあり、それ以外の言語では". "が文末にある事を想定
+    const delimiter = text.includes("。") ? "。" : ". ";
+    const lineSplits = text.split(delimiter);
+    const lines = [];
+
+    // 偶数行のみ改行を行う
+    for (let i = 0; i < lineSplits.length; i += lineSizes) {
+      const currentLines = lineSplits.slice(i, i + lineSizes);
+      // 最終行かどうか確認する
+      const isLastLines = i + lineSizes >= lineSplits.length;
+      // 最終チャンク以外の場合、かつ完全なペアであれば末尾にピリオドを追加
+      const extraPeriod =
+        !isLastLines && currentLines.length === lineSizes
+          ? delimiter.trim()
+          : "";
+      const pushLines = currentLines.join(delimiter) + extraPeriod;
+      lines.push(pushLines);
+    }
+    return lines;
+  };
+
   return (
     <div className="whitespace-break-spaces">
       {/* タイトル */}
@@ -15,7 +38,10 @@ const Paragraph: React.FC<QuestionParagraphProps | ReviewParagraphProps> = ({
       {/* 複数行対応の本文 */}
       <div className="ml-4 text-[16px] font-normal width_1440px:ml-5 width_1440px:text-[18px] width_1680px:ml-[22px] width_1680px:text-[20px]">
         {content &&
-          [paragraphContent].map((line, index) => <p key={index}>{line}</p>)}
+          paragraphContent &&
+          lineBreaks(paragraphContent).map((line, index) => (
+            <p key={index}>{line}</p>
+          ))}
       </div>
     </div>
   );
